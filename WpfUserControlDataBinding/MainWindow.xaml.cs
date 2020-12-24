e185b83fddbddb66a519897d6703ce857e7b08e0
Patrick <36152071+PatrickRainer@users.noreply.github.com>
2020-12-24 08:58:52 +0100

    Add files via upload

---
+++ WpfUserControlDataBinding/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfUserControlDataBinding.Annotations;
+
+namespace WpfUserControlDataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        string _windowPropertyText;
+
+        public string WindowPropertyText
+        {
+            get => _windowPropertyText;
+            set
+            {
+                if (value == _windowPropertyText) return;
+                _windowPropertyText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WindowPropertyText = "Enter Text here!";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
